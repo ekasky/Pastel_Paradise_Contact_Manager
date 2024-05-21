34d503944458ebc087b660c1b070c4887e44cfaa
Daryl Docteur <112511881+newgen211@users.noreply.github.com>
2024-05-21 14:59:49 -0400

    Add files via upload

---
+++ Create_Contact.php
@@ -0,0 +1,43 @@
+<?php
+$inData = getRequestInfo();
+
+$ID = $inData["ID"];
+$FirstName = $inData["FirstName"];
+$LastName = $inData["LastName"];
+$Phone = $inData["Phone"];
+$Email = $inData["Email"];
+$userId = $inData["userId"];
+
+$conn = new mysqli("localhost", "root", "", "contacts");
+
+if ($conn->connect_error) 
+{
+    returnWithError($conn->connect_error);
+} 
+else
+{
+    $stmt = $conn->prepare("INSERT into COP4431 (ID, FirstName, LastName, Phone, Email, UserID) VALUES(?,?,?,?,?,?)");
+    $stmt->bind_param("issssi", $ID, $FirstName, $LastName, $Phone, $Email, $userId);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    returnWithError("");
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+?>
