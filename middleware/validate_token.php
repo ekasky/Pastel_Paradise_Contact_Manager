ca64555f3dbab0ce1bf8876bb584ab07dceb192e
Evan Kasky <ekasky25@gmail.com>
2024-05-24 15:08:58 -0400

    Adds middlware to valide a token 	- Gets the secret key from env vars 	- Attempts to decode the token 	- If successfull returns true 	- Else logs error and returns false

---
+++ middleware/validate_token.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../vendor/autoload.php';
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+function validate_token($jwt) {
+
+    // Get the key from the env vars
+    $key                = getenv('SECRET_KEY');
+
+    // Try and decode the provied token
+    try {
+
+        $decoded        = JWT::decode($jwt, $key, ['HS256']);
+
+        return true;
+
+    }
+    catch(Exception $e) {
+
+        error_log('[ERROR] Could not validate the token: ' . $e->getMessage());
+        return false;
+
+    }
+
+}
+
+?>
