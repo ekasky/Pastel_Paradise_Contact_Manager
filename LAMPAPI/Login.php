300e3f40b9645a536c040e371fdb46e2e8e0b661
oatberries <oatmealjunk@gmail.com>
2024-06-01 17:55:45 -0400

    files in proper place and edits to html files

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -7,7 +7,8 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	//$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 
+	$conn = new mysqli('localhost', 'apiuser', 'Password1', 'COP4331');	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
