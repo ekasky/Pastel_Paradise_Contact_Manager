bc498b1a31b86133b656a6a7e68ff995ccf3fa82
Evan Kasky <ekasky25@gmail.com>
2024-06-05 12:46:40 -0400

    Client side validation for login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,36 @@
+const validateInput = (email, password) => {
+
+    let clientError = false;
+
+    // Grab the client slide vaidation error message div's by id
+    const email_client_error      = document.getElementById('email-error');
+    const password_client_error   = document.getElementById('password-error');
+
+    // Email validation
+
+    const emailPattern = /(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
+
+    if(!emailPattern.test(email)) {
+
+        // Set the error message
+        email_client_error.innerHTML = 'Invalid email format *';
+
+        // Remove the hidden class
+        email_client_error.classList.remove('hidden');
+
+        clientError = true;
+
+    }
+    else {
+
+        // Set the hidden class
+        email_client_error.classList.add('hidden');
+    }
+
+    return clientError;
+
+};
+
 // Get the html elements needed by id
 const form     = document.getElementById('login-form');
 const errorBox = document.getElementById('error-box');
@@ -11,6 +44,13 @@ form.addEventListener('submit', async (event) => {
     // Grab the form fields needed for login
     const email    = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+
+    // Client side form validation
+    if(validateInput(email, password) === true) {
+
+        return;
+
+    }
 
     // Prepare the request body
     const body = {email, password};
